af37b95ab9dd5a7995932bacffb32731362dfad1
logannick02 <logan8657@gmail.com>
2026-09-06 17:45:40 -0400

    updated valid answers list to be more inclusive

--- scripts/sanitize-script.py
+++ scripts/sanitize-script.py
@@ -1,8 +1,6 @@
 from wordfreq import word_frequency, zipf_frequency
 import json
 validAnswers = []
-validStarterWords = []
-
 
 with open("./scrabble-unsanitized.txt", "r") as file:
     content = file.read()
@@ -10,15 +8,9 @@ with open("./scrabble-unsanitized.txt", "r") as file:
     
     for word in content.split():
         # print(zipf_frequency(word, 'en'))
-        
-        if zipf_frequency(word, 'en') > 3.25:
-            validStarterWords.append(word)
-        
-        if zipf_frequency(word, 'en') > 2.25:
+              
+        if zipf_frequency(word, 'en') > 2.0:
             validAnswers.append(word)
     
-with open("starter.json", "w") as file:
-    json.dump(validStarterWords, file)
-
-with open("answers.json", "w") as file:
+with open("./src/data/answers.json", "w") as file:
     json.dump(validAnswers, file)
